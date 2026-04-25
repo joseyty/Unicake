31e984ce4ebe99eb5b18828b0d7ac6b1b0aba51b
Saynck <104885640+Saynck@users.noreply.github.com>
2026-04-25 08:23:18 -0300

    atualização

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Backend rodando 🚀');
+});
+
+app.listen(4000, () => {
+    console.log('Servidor rodando na porta 4000');
+});
